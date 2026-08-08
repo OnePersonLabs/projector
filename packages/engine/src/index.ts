968ffa0562351e55ef54a2948e6fa806fab794a9
Michael Jennings <8424394+michael-jennings@users.noreply.github.com>
2026-08-08 07:45:05 -0500

    feat: export coverage completion APIs

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -16,3 +16,4 @@ export * from "./governance/execution-policy.js";
 export * from "./governance/ignore-policy.js";
 export * from "./planning/revisions.js";
 export * from "./architecture/index.js";
+export * from "./coverage/index.js";
