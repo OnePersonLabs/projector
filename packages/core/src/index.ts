56328bbaaf1a5c6473fb207cedc4c4c7b5f33316
Michael Jennings <8424394+michael-jennings@users.noreply.github.com>
2026-09-10 12:59:27 -0500

    core: define application observation exchange

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -13,3 +13,4 @@ export * from "./schemas/change-proposal.js";
 export * from "./schemas/project-config.js";
 export * from "./schemas/operations.js";
 export * from "./schemas/project-data-migration.js";
+export * from "./schemas/application-observation.js";
